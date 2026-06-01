b208569d450733a3985927fed235d8460a6befab
Neco Kriel <necokriel@gmail.com>
2026-06-02 00:30:23 +0200

    update(path-aliases.sh): replace stale project aliases with repos group aliases.

--- configs/extras/pc/path-aliases.sh
+++ configs/extras/pc/path-aliases.sh
@@ -4,14 +4,9 @@
 
 create_alias_if_present --var DDWN  --alias ddwn  --dir "$DHOME/Downloads"
 
-create_alias_if_present --var DDOC  --alias ddoc  --dir "$HOME/Documents"
-create_alias_if_present --var DSYS  --alias dsys  --dir "$DDOC/SetupFramework"
-create_alias_if_present --var DNOTES --alias dnotes --dir "$DDOC/ProjectNotes"
-create_alias_if_present --var DBITES --alias dbites --dir "$DDOC/BiteSizePython"
-
-create_alias_if_present --var DPROJ  --alias dproj  --dir "$DHOME/Projects"
-create_alias_if_present --var DDOT   --alias ddot   --dir "$DPROJ/DotFiles"
-create_alias_if_present --var DASGARD --alias dasgard --dir "$DPROJ/Asgard"
-create_alias_if_present --var DQUOKKA --alias dquokka --dir "$DPROJ/quokka"
+create_alias_if_present --var DREPOS --alias drepos --dir "$DHOME/repos"
+create_alias_if_present --var DRES   --alias dres   --dir "$DREPOS/research"
+create_alias_if_present --var DSYS   --alias dsys   --dir "$DREPOS/system"
+create_alias_if_present --var DTEACH --alias dteach --dir "$DREPOS/teaching"
 
 ## .
