a1b2666f95949b7f0492e592c74fb1a585860def
Neco Kriel <necokriel@gmail.com>
2026-06-13 16:27:58 +0200

    config(path-aliases.sh): rename drnotes to dpnotes; update dir to repos/ProjectNotes.

--- configs/extras/pc/path-aliases.sh
+++ configs/extras/pc/path-aliases.sh
@@ -7,7 +7,7 @@ create_alias_if_present --var DDWN --alias ddwn --dir "$DHOME/Downloads"
 create_alias_if_present --var DREPOS --alias drepos --dir "$DHOME/repos"
 
 create_alias_if_present --var DRES --alias dres --dir "$DREPOS/research"
-create_alias_if_present --var DRNOTES --alias drnotes --dir "$DRES/ResearchNotes"
+create_alias_if_present --var DPNOTES --alias dpnotes --dir "$DREPOS/ProjectNotes"
 create_alias_if_present --var DASGARD --alias dasgard --dir "$DRES/Asgard"
 create_alias_if_present --var DQUOKKA --alias dquokka --dir "$DRES/quokka"
 create_alias_if_present --var DPENCIL --alias dpencil --dir "$DRES/pencil-code"
