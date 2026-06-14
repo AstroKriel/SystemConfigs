db7e8e3b95d38e199fd15c540272568df5ccfb8f
Neco Kriel <necokriel@gmail.com>
2026-06-14 12:45:55 +0200

    config(path-aliases.sh): replace DRES/research with DCODE/codebases; update DQUOKKA and DPENCIL paths.

--- configs/extras/pc/path-aliases.sh
+++ configs/extras/pc/path-aliases.sh
@@ -6,11 +6,12 @@ create_alias_if_present --var DDWN --alias ddwn --dir "$DHOME/Downloads"
 
 create_alias_if_present --var DREPOS --alias drepos --dir "$DHOME/repos"
 
-create_alias_if_present --var DRES --alias dres --dir "$DREPOS/research"
 create_alias_if_present --var DPNOTES --alias dpnotes --dir "$DREPOS/ProjectNotes"
-create_alias_if_present --var DASGARD --alias dasgard --dir "$DRES/Asgard"
-create_alias_if_present --var DQUOKKA --alias dquokka --dir "$DRES/quokka"
-create_alias_if_present --var DPENCIL --alias dpencil --dir "$DRES/pencil-code"
+create_alias_if_present --var DASGARD --alias dasgard --dir "$DREPOS/Asgard"
+
+create_alias_if_present --var DCODE --alias dcode --dir "$DREPOS/codebases"
+create_alias_if_present --var DQUOKKA --alias dquokka --dir "$DCODE/quokka"
+create_alias_if_present --var DPENCIL --alias dpencil --dir "$DCODE/pencil-code"
 
 create_alias_if_present --var DSYS --alias dsys --dir "$DREPOS/system"
 create_alias_if_present --var DSCONFIGS --alias dsconfigs --dir "$DSYS/SystemConfigs"
