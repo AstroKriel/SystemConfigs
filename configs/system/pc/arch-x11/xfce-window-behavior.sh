def78f484def9644030f3c52c9cfa81894acc028
Neco Kriel <necokriel@gmail.com>
2026-06-16 02:32:27 +0200

    del(arch-x11/xfce-window-behavior.sh): document in arch-install.md; not a deployable config.

--- configs/system/pc/arch-x11/xfce-window-behavior.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-
-# Switch to an application's existing workspace when it requests activation
-# instead of moving that application window to the current workspace.
-xfconf-query -c xfwm4 -p /general/activate_action -s switch
